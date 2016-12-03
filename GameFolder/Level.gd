c778a1bcb532f9ce46c777310ea5c13c4d869bea
Benoît Roman <benoit.roman@gmail.com>
2016-12-03 17:31:39 +0100

    implemented the character's moves including the box being pushed

--- GameFolder/Level.gd
+++ GameFolder/Level.gd
@@ -23,12 +23,19 @@ var Layout = ["############",
 #			"    #     #########",
 #			"    #######"]
 
+# Tiles IDs used to setup floor and items tilemap
 const NONE = -1
 const WALL = 0
 const FLOOR = 1
 const CRATE = 2
 const TARGET = 3
 const PLAYER = 4
+
+# Direction constants
+const UP = 0
+const RIGHT = 1
+const DOWN = 2
+const LEFT = 3
 
 var player_pos = []
 var x_max = 0
@@ -78,6 +85,7 @@ func lay_floor():
 func set_walls_and_items():
 	var fl = get_node("FloorLayout")
 	var items = get_node("Items")
+	var initItems = get_node("InitialItems")
 	var ts = fl.get_tileset()
 	var y = 0
 	for iter in Layout:
@@ -92,7 +100,7 @@ func set_walls_and_items():
 				type = CRATE
 			elif char == "@": # player
 				type = PLAYER
-				player_pos =[x, y]
+				player_pos = [x, y]
 			elif char == ".": # target
 				type = TARGET
 			
@@ -100,6 +108,7 @@ func set_walls_and_items():
 				fl.set_cell(x, y, WALL)
 			else:
 				items.set_cell(x, y, type)
+				initItems.set_cell(x, y, type) # this copy will not be modified and will hold places of the target
 			if x > x_max:
 				x_max = x
 			x += 1
@@ -110,17 +119,86 @@ func set_walls_and_items():
 func _ready():
 	set_walls_and_items()
 	lay_floor()
-	set_process(true)
+	set_process_input(true)
 
-func _process(delta):
+func _input(event):
 	var fl = get_node("FloorLayout")
 	var ts = fl.get_tileset()
 	var items = get_node("Items")
 	
-	#if Input.is_action_pressed("player_down") and fl.get_cell(player_pos[0], player_pos[1]) == FLOOR:
-	#	
+	var origin = get_node("InitialItems").get_cell(player_pos[0], player_pos[1])
+	if origin != TARGET:
+		origin = NONE
+	
+	if event.is_action_released("player_down") and is_move_possible(player_pos, DOWN):
+		#print ("input player down")
+		# remove character from old location
+		get_node("Items").set_cell(player_pos[0], player_pos[1], origin)
+		# update game state
+		player_pos[1] += 1
+		# push box if needed
+		if items.get_cell(player_pos[0], player_pos[1]) == CRATE:
+			items.set_cell(player_pos[0], player_pos[1]+1, CRATE)
+	
+	if event.is_action_released("player_up") and is_move_possible(player_pos, UP):
+		#print ("input player up")
+		# remove character from old location
+		get_node("Items").set_cell(player_pos[0], player_pos[1], origin)
+		# update game state
+		player_pos[1] -= 1
+		# push box if needed
+		if items.get_cell(player_pos[0], player_pos[1]) == CRATE:
+			items.set_cell(player_pos[0], player_pos[1]-1, CRATE)
+	
+	if event.is_action_released("player_right") and is_move_possible(player_pos, RIGHT):
+		#print ("input player right")
+		# remove character from old location
+		get_node("Items").set_cell(player_pos[0], player_pos[1], origin)
+		# update game state
+		player_pos[0] += 1
+		# push box if needed
+		if items.get_cell(player_pos[0], player_pos[1]) == CRATE:
+			items.set_cell(player_pos[0]+1, player_pos[1], CRATE)
+	
+	if event.is_action_released("player_left") and is_move_possible(player_pos, LEFT):
+		#print ("input player left")
+		# remove character from old location
+		get_node("Items").set_cell(player_pos[0], player_pos[1], origin)
+		# update game state
+		player_pos[0] -= 1
+		# push box if needed
+		if items.get_cell(player_pos[0], player_pos[1]) == CRATE:
+			items.set_cell(player_pos[0]-1, player_pos[1], CRATE)
+	
+	# move char sprite in the destination
+	get_node("Items").set_cell(player_pos[0], player_pos[1], PLAYER)
 
 
+func is_move_possible(origin, direction, move_crate=false):
+	if direction < 0 or direction > 3:
+		return false # invalid argument
+	var destination = [origin[0], origin[1]]
+	if direction == UP:
+		destination[1] -= 1
+	elif direction == RIGHT:
+		destination[0] += 1
+	elif direction == DOWN:
+		destination[1] += 1
+	elif direction == LEFT:
+		destination[0] -= 1
+	if destination[0] < 0 or destination[0] > x_max:
+		return false # cannot move outside of the game area
+	if destination[1] < 0 or destination[1] > x_max:
+		return false # cannot move outside of the game area
+	if get_node("FloorLayout").get_cell(destination[0], destination[1]) == WALL:
+		return false # cannot move into a wall
+	if move_crate and get_node("Items").get_cell(destination[0], destination[1]) == CRATE:
+		return false # cannot move a crate in another crate
+	if get_node("Items").get_cell(destination[0], destination[1]) == CRATE and not is_move_possible(destination, direction, true):
+		# check if the crate itself can be moved in the same direction and doesn't hit another crate
+		return false # destination crate cannot be moved
+	#if all other test succeeded, move allowed
+	return true
 
 
 
